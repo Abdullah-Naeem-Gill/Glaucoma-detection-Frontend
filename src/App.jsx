e1dfcf9a47eb89ab3369daa27394a475e98c2dc6
Abdullah Naeem <abdullahnaaemgill1724@gmail.com>
2025-05-02 19:36:16 +0500

    added changes to app.jsx and apply some fixs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import React from "react";
-
+import React from 'react'
+import Header from './components/Header'
+import NavBar from './components/NavBar'
+import HeroSection from './components/HeroSection'
+import Service from './components/Service'
+import Appointment from './components/Appointment'
+import Footer from './components/Footer'
 const App = () => {
-  return <div className="text-blue-700">hello</div>;
-};
+  return (
+    <div>
+      
+      <Header/>
+    <HeroSection/>
+    <Service/>
+    <Appointment /> 
+    <Footer />
+    </div>
+  )
+}
 
-export default App;
+export default App
